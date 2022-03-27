5066ea18a310e0ea60c46d351a1b6062d5e80b21
Marc <marcsanchez@me.com>
2022-03-26 20:43:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+/*
+- Create a function called wrapAdjective()
+  - Give it a str parameter that defaults to "*"
+  - Return a function
+   - Takes a single parameter that defaults to "special"
+   - Return a string. "You are *a param programmer*!"
+*/
+
+function wrapAdjective(str = "*") {
+  let inner = (param = "special") => {
+    return `You are ${str}${param}${str}!`
+  }
+  return inner;
+}
+
+
+console.log(wrapAdjective("%")("a dedicated programmer"));
